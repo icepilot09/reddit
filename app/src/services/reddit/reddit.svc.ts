72d3721762626089bbd90b6d35fe04cc0e1862ee
[Michael] <michael@lewiscommunications.com>
2015-10-08 12:09:57 -0500

    added host and started GET

--- app/src/services/reddit/reddit.svc.ts
+++ app/src/services/reddit/reddit.svc.ts
@@ -2,7 +2,14 @@ import {async, register} from 'platypus';
 import BaseService from '../base/base.svc';
 
 export default class RedditService extends BaseService {
-
+    getPosts(): async.IThenable<Array<any>> {
+        return this.http.json<Array<any>>({
+            method: 'GET',
+            url: this.host
+        }).then((success) => {
+            var res: any = success;
+            return <Array<any>>res.response.results;
+        });
+    }
 }
-
 register.injectable('reddit-svc', RedditService);
